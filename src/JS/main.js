420b5616a46061c1d2d30943f41bdba6c78f3e00
Jenny Lind <jeli2308@student.miun.se>
2024-01-26 13:12:44 +0100

    Ändrat header till enkel h1, lagt till en sök-knapp samt tagit bort funktionen showHeading och ersatt med showTable som än så länge är tom

--- src/JS/main.js
+++ src/JS/main.js
@@ -5,19 +5,11 @@ window.onload = init;
 
 // Funktion som körs direkt vid sidladdning.
 function init() {
-    // Visar rubrik.
-    showHeading();
+    // Visar tabell.
+    showTable();
 }
 
-function showHeading() {
-    // Skapar ny variabel.
-    let headingEl = document.getElementById("heading");
-
-    // Skapar ny rubrik med utskriftsformat <h1>Rubrik</h1>.
-    let newHeadingEl = document.createElement("h1");
-    let newHeadingText = document.createTextNode("Webbutveckling - Ramschema");
-    newHeadingEl.appendChild(newHeadingText);
-
-    // Skriver ut till DOM och rubriken läggs till.
-    headingEl.appendChild(newHeadingEl)
+// Skriver ut tabell med kursinformation.
+function showTable() {
+    
 }
